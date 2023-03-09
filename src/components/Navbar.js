271cb9d81df010a0c76decef33dd9e3e4eba0763
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-09 19:48:31 +0100

    Modify changes in Navbar.js file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav>
-    <h1>Bookstore CMS</h1>
+  <div>
+    <nav>
+      <h1>Bookstore CMS</h1>
 
-    <ul>
-      <li>
-        <Link to="/">BOOKS</Link>
-      </li>
+      <ul>
+        <li>
+          <Link to="/">BOOKS</Link>
+        </li>
 
-      <li>
-        <Link to="/categories">CATEGORIES</Link>
-      </li>
+        <li>
+          <Link to="/categories">CATEGORIES</Link>
+        </li>
 
-    </ul>
-  </nav>
+      </ul>
+    </nav>
+  </div>
 );
 
 export default Navbar;
