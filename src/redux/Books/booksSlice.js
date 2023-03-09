cb449f0f2d86df1902bc51ab43c984601896d665
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-09 09:18:31 +0100

    Unstall npm install --save redux-immer

--- src/redux/Books/booksSlice.js
+++ src/redux/Books/booksSlice.js
@@ -1,42 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
-import produce from 'immer';
 
-const initialState = {
-  books: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
-};
+const initialState = [
+  {
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
 
 const bookSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    addBook: (state, action) => produce(state, (draftState) => {
-      draftState.books.push(action.payload);
-    }),
+    addBook: (state, action) => (state, action) => {
+      state.books.push(action.payload);
+    },
 
     removeBook: (state, action) => {
       const { id } = action.payload;
-      return produce(state, (draftState) => {
-        draftState.books = draftState.books.filter((book) => book.item_id !== id);
-      });
+      
+      state.books.filter((book) => book.item_id !== id);
     },
   },
 });
